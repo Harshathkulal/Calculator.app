849af0864a70c1930e643279acedf9b83cb7b5c5
harshath <harshathmkulal@email.com>
2023-08-03 22:49:23 +0530

    added comments

--- calc.js
+++ calc.js
@@ -2,14 +2,17 @@ let string="";
 let buttons=document.querySelectorAll(".button");
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
+        //If pressed =
         if(e.target.innerHTML=='='){
             string=eval(string);
             document.querySelector("input").value=string;
         }
+          //If pressed C
         else if(e.target.innerHTML=='C'){
             string="";
             document.querySelector("input").value=string;
         }
+          //If pressed DEL
         else if(e.target.innerHTML=='DEL'){
             string=string.toString().slice(0,-1);
             document.querySelector("input").value=string;
